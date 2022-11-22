857f0a3353783bf916e90bdf0be224f10f9b94e8
shoaibzaheer95 <shoaibzaheer95@gmail.com>
2022-11-22 22:16:07 +0500

    logo set

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header(){
     return(
     <div className="header">
-        <img src="./images/logo.png" alt=""/>
+        <img src="./images/logo.png" alt=""/>  my travel journal.
     </div>
     )
 }
